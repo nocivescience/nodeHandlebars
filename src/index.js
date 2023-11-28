2b944d7b901342dcec88590a0b1b28bc5dd5508f
nocive.science <nocive.science@gmail.com>
2023-11-28 04:34:40 -0300

    no funciona el navegador

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const {engine} = require('express-handlebars');
+const port = 3000;
+app.set('views', './src/views');
+app.engine('.hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'main',
+}));
+app.get('/', (req, res) => {
+    const data ={
+        name: 'John Doe',
+        message: 'Hello World',
+    }
+    res.render('home', data);
+});
+app.set('view engine', '.hbs');
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/views/partials/navegador.hbs
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">Sobre</a></li>
+        <li><a href="/contact">Contato</a></li>
+    </ul>
+</nav>
