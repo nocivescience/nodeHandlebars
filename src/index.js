effc3e2d89efbef59bc550cf722729d5b153e94f
nocive.science <nocive.science@gmail.com>
2023-11-28 02:32:06 -0300

    cambiaré a modulos

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const {engine} = require('express-handlebars');
+const port = 3000;
+app.set('views', './src/views');
+app.set('.hbs', engine({
+    defaultLayout: 'main',
+    extname: '.hbs',
+}))
+app.set('view engine', '.hbs');
+app.get('/', (req, res) => {
+  const data= {
+    title: 'Express Handlebars',
+    message: 'This is a message',
+  }
+  res.render('index', data);
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
