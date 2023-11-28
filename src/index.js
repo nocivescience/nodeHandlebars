c5425b7ea1856965194c99d8e28e280ab3e34825
nocive.science <nocive.science@gmail.com>
2023-11-28 05:09:37 -0300

    al parecer funciona pero no se muestra el cuerpo

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ const app = express();
 const port = 3000;
 const {engine} = require('express-handlebars');
 app.set('views', './src/views');
-app.set('view engine', 'handlebars');
-app.engine('hbs', engine({
+app.engine('.hbs', engine({
     defaultLayout: 'main',
     extname: '.hbs',
 }));
+app.set('view engine', '.hbs');
+app.get('/', (req, res) => {
+    const data = {
+        name: 'World',
+        text: 'Hello',
+    };
+    res.render('home', data);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
