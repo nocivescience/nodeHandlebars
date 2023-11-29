b336cf8055d7fd99eaa76b46c61a89bc836ca8be
nocive.science <nocive.science@gmail.com>
2023-11-29 06:03:01 -0300

    esto ya funciona pero no como yo quería

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import {engine} from 'express-handlebars';
 import path from 'path';
-import {query} from './db';
+// import {query} from './db';
+import pgk from 'pg';
 const app = express();
 const port = 3000;
+const { Pool } = pgk;
+const pool= new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  port: '5432',
+  database: 'postgres',
+  password: 'comenius12',
+})
 app.set('views', './src/views');
 app.engine('.hbs', engine({
     defaultLayout: 'main',
@@ -11,7 +20,7 @@ app.engine('.hbs', engine({
 }))
 app.set('view engine', '.hbs');
 app.get('/', async(req, res) => {
-  const result= await query('SELECT * FROM users');
+  const result= await pool.query('SELECT * FROM cuadernos');
   res.render('index', {result: result.rows});
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
