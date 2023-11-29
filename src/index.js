1a10db3e9d988410b8c749c86b7695d5448b614b
nocive.science <nocive.science@gmail.com>
2023-11-29 19:40:24 -0300

    terminado con base y formulario conectado

--- src/index.js
+++ src/index.js
@@ -9,8 +9,20 @@ app.engine('.hbs', engine({
     extname: '.hbs',
 }));
 app.set('view engine', '.hbs');
+app.use(express.urlencoded({extended: false}));
 app.get('/', async(req, res) => {
     const data = await db.query('SELECT * FROM cuadernos');
     res.render('index', {data: data.rows});
+});
+app.post('/mi_formulario', async(req, res) => {
+    const {mi_asignatura, mi_nota, mi_titulo}=req.body;
+    try {
+        await db.query('INSERT INTO cuadernos (asignatura, nota, titulo) VALUES ($1, $2, $3)', [mi_asignatura, mi_nota, mi_titulo]);
+        res.redirect('/');
+        console.log('Cuaderno guardado');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error al guardar el cuaderno');
+    }
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
