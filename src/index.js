65b3954a7136bb867f25a91d4501e7e9c7863713
nocive.science <nocive.science@gmail.com>
2023-11-29 21:14:34 -0300

    listo el formulario y la base de datos

--- src/index.js
+++ src/index.js
@@ -9,8 +9,19 @@ app.engine('.hbs', engine({
     extname: '.hbs',
 }))
 app.set('view engine', '.hbs');
+app.use(express.urlencoded({extended: true}));
 app.get('/', async(req, res) => {
   const resultados= await db.query('SELECT * FROM cuadernos');  
   res.render('index', {resultados: resultados.rows});
 });
+app.post('/form', async(req, res) => {
+    const {asignatura, nota, titulo}= req.body;
+    try{
+      await db.query('INSERT INTO cuadernos (asignatura, nota, titulo) VALUES ($1, $2, $3)', [asignatura, nota, titulo]);
+      res.redirect('/');
+      console.log('datos guardados');
+    }catch(error){
+      console.log(`el error esta aca: ${error}`);
+    }
+}); 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
